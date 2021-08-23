fb7cbf6858a74192f1571ee2654fb6ce6dcd2811
sehdevkumar <sehdev@atai.ai>
2021-08-23 18:23:13 +0530

    second commit

--- src/app/guards/auth/auth.guard.ts
+++ src/app/guards/auth/auth.guard.ts
@@ -15,7 +15,7 @@ export class AuthGuard implements CanActivate,CanActivateChild {
   canActivateChild(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return false;
+    return true;
   }
   
 }
